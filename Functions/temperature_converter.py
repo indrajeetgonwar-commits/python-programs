18027756bea9a6fe4b34ce86f6b42a12828e0ed6
Indrajeet <indrajeetgonwar@gmail.com>
2026-07-02 12:49:49 +0530

    Create temperature_converter.py

---
+++ Functions/temperature_converter.py
@@ -0,0 +1,7 @@
+# Celsius to Fahrenheit Using Function
+
+def celsius_to_fahrenheit(c):
+    return (c * 9 / 5) + 32
+
+c = float(input("Enter Celsius: "))
+print("Fahrenheit =", celsius_to_fahrenheit(c))
